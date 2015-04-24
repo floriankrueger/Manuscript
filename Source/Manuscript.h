79d52ab80ba410bcf41c804a332ecb409671180f
Florian Krüger <florian.krueger@projectserver.org>
2015-04-24 08:31:51 +0200

    [POD] basic workspace setup

---
+++ Source/Manuscript.h
@@ -0,0 +1,19 @@
+//
+//  Manuscript.h
+//  Manuscript
+//
+//  Created by Florian Krüger on 24/04/15.
+//  Copyright (c) 2015 projectserver.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Manuscript.
+FOUNDATION_EXPORT double ManuscriptVersionNumber;
+
+//! Project version string for Manuscript.
+FOUNDATION_EXPORT const unsigned char ManuscriptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Manuscript/PublicHeader.h>
+
+
