1085327456106bfcc61f40e3f4af0d20f4e96d2c
Florian Krüger <florian.krueger@projectserver.org>
2015-04-25 22:03:30 +0200

    [POD] made Manuscript scheme shared

--- Example/Examples/SimpleButtonExample/SimpleButtonExampleViewController.swift
+++ Example/Examples/SimpleButtonExample/SimpleButtonExampleViewController.swift
@@ -7,8 +7,24 @@
 //
 
 import UIKit
+import Manuscript
 
 class SimpleButtonExampleViewController: UIViewController {
+
+  // MARK: Private Properties
+
+  private let activeButton: UIButton? = nil
+
+  private let optionA: UIButton = UIButton.buttonWithType(.System) as! UIButton
+  private let optionB: UIButton = UIButton.buttonWithType(.System) as! UIButton
+  private let optionC: UIButton = UIButton.buttonWithType(.System) as! UIButton
+
+  private var optionAHeight: NSLayoutConstraint? = nil
+  private var optionAWidth: NSLayoutConstraint? = nil
+  private var optionBHeight: NSLayoutConstraint? = nil
+  private var optionBWidth: NSLayoutConstraint? = nil
+  private var optionCHeight: NSLayoutConstraint? = nil
+  private var optionCWidth: NSLayoutConstraint? = nil
 
   // MARK: - Init
 
@@ -26,20 +42,63 @@ class SimpleButtonExampleViewController: UIViewController {
   override func loadView() {
     super.loadView()
 
-    self.view.backgroundColor = UIColor.whiteColor()
+    self.view.backgroundColor = UIColor.grayColor()
 
     self.setupSubviews()
     self.setupLayout()
   }
 
+  // MARK: - Target
+
+  func buttonPressed(sender: UIButton) {
+    if sender == self.activeButton {
+      return;
+    }
+
+    if let activeButton = self.activeButton {
+      self.shrinkButton(activeButton)
+    }
+  }
+
   // MARK: - Setup & Layout
 
   private func setupSubviews() {
+    self.optionA.backgroundColor = UIColor.whiteColor()
+    self.optionA.setTitle("A", forState: .Normal)
+    self.optionA.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+    self.view.addSubview(self.optionA)
 
+    self.optionB.backgroundColor = UIColor.whiteColor()
+    self.optionB.setTitle("B", forState: .Normal)
+    self.optionB.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+    self.view.addSubview(self.optionB)
+
+    self.optionC.backgroundColor = UIColor.whiteColor()
+    self.optionC.setTitle("C", forState: .Normal)
+    self.optionC.addTarget(self, action: "buttonPressed:", forControlEvents: .TouchUpInside)
+    self.view.addSubview(self.optionC)
   }
 
   private func setupLayout() {
+    Manuscript.layout(self.optionA) { c in
+      self.optionAHeight = c.set(.Height, to:60.0).constraint
+      self.optionAWidth = c.set(.Width, to:60.0).constraint
+      c.make(.Right, equalTo:self.optionB, s:.Left, minus: 10.0)
+      c.make(.CenterY, equalTo:self.view, s:.CenterY)
+    }
 
+    Manuscript.layout(self.optionB) { c in
+      self.optionBHeight = c.set(.Height, to:60.0).constraint
+      self.optionBWidth = c.set(.Width, to:60.0).constraint
+      c.centerIn(self.view)
+    }
+
+    Manuscript.layout(self.optionC) { c in
+      self.optionCHeight = c.set(.Height, to:60.0).constraint
+      self.optionCWidth = c.set(.Width, to:60.0).constraint
+      c.make(.Left, equalTo:self.optionB, s:.Right, plus: 10.0)
+      c.make(.CenterY, equalTo:self.view, s:.CenterY)
+    }
   }
 
 }
